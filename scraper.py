11228f53c04ffcf3eb1d3167cbf4b54683d8c7d5
Watt3r <git@lucas.tools>
2024-01-13 16:53:06 +0000

    no more offset

--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-today = datetime.date.today() + datetime.timedelta(days=3)
+today = datetime.date.today()
 
 ntfy_key = os.getenv("NTFY_KEY")
 username = os.getenv("USERNAME")
